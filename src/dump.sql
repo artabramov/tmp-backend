7b939810cc339c34627150c02805e60bf551bfa6
Artem Abramov <notdepot@gmail.com>
2021-05-20 19:07:19 +0000

    updated

--- src/dump.sql
+++ src/dump.sql
@@ -271,3 +271,163 @@ DELETE FROM hubs;
 DELETE FROM users;
 
 SELECT * FROM users; SELECT * FROM hubs; SELECT * FROM hub_meta; SELECT * FROM user_roles; SELECT * FROM user_meta; SELECT * FROM posts; SELECT * FROM post_meta; SELECT * FROM post_tags; SELECT * FROM post_comments;
+
+
+# ==========================================================================================================
+
+
+SET sql_mode = '';
+CREATE TABLE IF NOT EXISTS users (
+    id           BIGINT(20)   UNSIGNED NOT NULL AUTO_INCREMENT,
+    create_date  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date  DATETIME     NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    restore_date DATETIME     NOT NULL DEFAULT '0000-00-00 00:00:00',
+    auth_date    DATETIME     NOT NULL DEFAULT '0000-00-00 00:00:00',
+    user_status  ENUM('pending', 'approved', 'trash') NOT NULL,
+    user_token   CHAR(80)     NOT NULL,
+    user_email   VARCHAR(255) NOT NULL,
+    user_name    VARCHAR(255) NOT NULL,
+    user_hash    VARCHAR(40)  NOT NULL DEFAULT '',
+
+    PRIMARY KEY (id),
+            KEY (create_date),
+            KEY (update_date),
+            KEY (restore_date),
+            KEY (auth_date),
+            KEY (user_status),
+     UNIQUE KEY (user_token),
+     UNIQUE KEY (user_email),
+            KEY (user_name),
+            KEY (user_hash)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+SET sql_mode = '';
+CREATE TABLE IF NOT EXISTS hubs (
+    id          BIGINT(20)   UNSIGNED NOT NULL AUTO_INCREMENT,
+    create_date DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date DATETIME     NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    user_id     BIGINT(20)   UNSIGNED NOT NULL,
+    hub_status  ENUM('custom', 'trash') NOT NULL,
+    hub_name    VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id),
+            KEY (create_date),
+            KEY (update_date),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION,
+            KEY (hub_status),
+            KEY (hub_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+SET sql_mode = '';
+CREATE TABLE IF NOT EXISTS roles (
+    id          BIGINT(20)  UNSIGNED NOT NULL AUTO_INCREMENT,
+    create_date DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date DATETIME    NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    user_id     BIGINT(20)  UNSIGNED NOT NULL,
+    hub_id      BIGINT(20)  UNSIGNED NOT NULL,
+    user_role  ENUM('admin', 'author', 'editor', 'reader', 'none') NOT NULL,
+
+        PRIMARY KEY (id),
+                KEY (create_date),
+                KEY (update_date),
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION,
+        FOREIGN KEY (hub_id) REFERENCES hubs (id) ON DELETE CASCADE,
+         UNIQUE KEY (user_id, hub_id),
+                KEY (user_role)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+SET sql_mode = '';
+CREATE TABLE IF NOT EXISTS posts (
+    id            BIGINT(20)  UNSIGNED NOT NULL AUTO_INCREMENT,
+    create_date   DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date   DATETIME    NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    user_id       BIGINT(20)  UNSIGNED NOT NULL,
+    hub_id        BIGINT(20)  UNSIGNED NOT NULL,
+    post_type     ENUM('document') NOT NULL,
+    post_status   ENUM('todo', 'doing', 'done', 'trash') NOT NULL,
+    post_excerpt  VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id),
+            KEY (create_date),
+            KEY (update_date),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION,
+    FOREIGN KEY (hub_id) REFERENCES hubs (id) ON DELETE CASCADE,
+            KEY (post_type),
+            KEY (post_status),
+            KEY (post_excerpt)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+SET sql_mode = '';
+CREATE TABLE IF NOT EXISTS comments (
+    id           BIGINT(20)  UNSIGNED NOT NULL AUTO_INCREMENT,
+    create_date  DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date  DATETIME    NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    user_id      BIGINT(20)  UNSIGNED NOT NULL,
+    post_id      BIGINT(20)  UNSIGNED NOT NULL,
+    comment_text TEXT        NOT NULL,
+
+    PRIMARY KEY (id),
+            KEY (create_date),
+            KEY (update_date),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION,
+    FOREIGN KEY (post_id) REFERENCES posts (id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+SET sql_mode = '';
+CREATE TABLE IF NOT EXISTS uploads (
+    id            BIGINT(20)   UNSIGNED NOT NULL AUTO_INCREMENT,
+    create_date   DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date   DATETIME     NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    user_id       BIGINT(20)   UNSIGNED NOT NULL,
+    comment_id    BIGINT(20)   UNSIGNED NULL,
+    upload_name   VARCHAR(255) NOT NULL,
+    upload_mime   VARCHAR(255) NOT NULL,
+    upload_size   BIGINT(20)   NOT NULL,
+    upload_file   VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (id),
+            KEY (create_date),
+            KEY (update_date),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION,
+    FOREIGN KEY (comment_id) REFERENCES comments (id) ON DELETE SET NULL,
+            KEY (upload_name),
+            KEY (upload_mime),
+            KEY (upload_size),
+     UNIQUE KEY (upload_file)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+SET sql_mode = '';
+CREATE TABLE IF NOT EXISTS meta (
+    id          BIGINT(20)   UNSIGNED NOT NULL AUTO_INCREMENT,
+    create_date DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_date DATETIME     NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    user_id     BIGINT(20)   UNSIGNED NOT NULL,
+    parent_type ENUM('users', 'hubs', 'roles', 'posts', 'comments', 'uploads') NOT NULL,
+    parent_id   BIGINT(20)   UNSIGNED NOT NULL,
+    meta_key    VARCHAR(20)  NOT NULL,
+    meta_value  VARCHAR(255) NOT NULL DEFAULT '',
+
+    PRIMARY KEY (id),
+            KEY (create_date),
+            KEY (update_date),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE NO ACTION,
+            KEY (parent_type),
+            KEY (parent_id),
+            KEY (meta_key),
+            KEY (meta_value)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS meta;
+DROP TABLE IF EXISTS uploads;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS hubs;
+DROP TABLE IF EXISTS users;
