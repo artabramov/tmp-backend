40a01248341857fd82fde81c843772763b120804
Artem Abramov <notdepot@gmail.com>
2021-05-24 19:50:35 +0000

    updated

---
+++ public/navbar.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+    <li class="nav-item">
+        <a class="nav-link" href="#">Users</a>
+    </li>
+
+    <li class="pl-2 nav-item">
+        <a class="nav-link" href="#">Hubs</a>
+    </li>
+
+    <li class="pl-2 nav-item">
+        <a class="nav-link" href="#">Docs <span class="badge badge-light">+2</span></a>
+    </li>
+
+
+
+
+
+
+    <li class="pl-5 nav-item">
+        <a href="#" class="btn btn-outline-light" type="submit"><i class="material-icons">add_circle_outline</i> Add document</a>
+    </li>
+
+
+
+
+
+
+      
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+        <a class="nav-link">Sign out</a>
+        </li>
+    </ul>
+
+  </div>
+</nav>
