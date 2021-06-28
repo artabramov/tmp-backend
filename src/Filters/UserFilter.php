f085a7371affd0d582d60f4f90323c032caf4717
vagrant <vagrant@vagrant.vm>
2021-06-28 18:54:33 +0000

    updated

---
+++ src/Filters/UserFilter.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Filters;
+
+class UserFilter
+{
+    protected $error;
+    protected $em;
+    protected $user;
+
+    public function __construct($em, \App\Entities\User $user) {
+        $this->error = '';
+        $this->em = $em;
+        $this->user = $user;
+    }
+
+    public function __set( $key, $value ) {
+        if( property_exists( $this, $key )) {
+            $this->$key = $value;
+        }
+    }
+
+    public function __get( $key ) {
+        if( property_exists( $this, $key )) {
+            return $this->$key;
+        }
+        return null;
+    }
+
+    public function __isset( $key ) {
+        if( property_exists( $this, $key )) {
+            return !empty( $this->$key );
+        }
+        return false;
+    }
+
+
+    // User register
+    public function insert() {
+        if(empty($this->user_name)) {
+            $this->error = 'error!';
+        }
+    }
+
+    public function update() {}
+
+    public function delete() {}
+
+    public function select($args) {}
+
+}
