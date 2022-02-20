d28ebfff6f432b6b2c78e77c386691b5db03fdd5
Artem Abramov <notdepot@gmail.com>
2022-02-20 17:22:49 +0000

    updated

--- app/core/log_wrapper.py
+++ app/core/log_wrapper.py
@@ -1,14 +1,14 @@
 from flask import request, has_request_context
 import logging
-#import os
-#import pwd, grp
+import os
+import pwd, grp
 
 def log_wrapper(app):
-    #if not os.path.isfile(app.config['LOG_FILENAME']):
-    #    open(app.config['LOG_FILENAME'], 'a').close()
-    #    uid = pwd.getpwnam('www-data').pw_uid
-    #    gid = grp.getgrnam('root').gr_gid
-    #    os.chown(app.config['LOG_FILENAME'], uid, gid)
+    if not os.path.isfile(app.config['LOG_FILENAME']):
+        open(app.config['LOG_FILENAME'], 'a').close()
+        #uid = pwd.getpwnam('www-data').pw_uid
+        #gid = grp.getgrnam('root').gr_gid
+        #os.chown(app.config['LOG_FILENAME'], uid, gid)
 
     #class ContextualFilter(logging.Filter):
     #    def filter(self, message):
@@ -20,15 +20,15 @@ def log_wrapper(app):
     #            message.method = '-'
     #        return True
 
-    #while app.logger.hasHandlers():
-    #    app.logger.removeHandler(app.logger.handlers[0])
+    while app.logger.hasHandlers():
+        app.logger.removeHandler(app.logger.handlers[0])
 
-    #handler = logging.handlers.TimedRotatingFileHandler(
-    #    filename=app.config['LOG_FILENAME'], 
-    #    when=app.config['LOG_ROTATE_WHEN'], 
-    #    backupCount=app.config['LOG_BACKUP_COUNT'])
+    handler = logging.handlers.TimedRotatingFileHandler(
+        filename=app.config['LOG_FILENAME'], 
+        when=app.config['LOG_ROTATE_WHEN'], 
+        backupCount=app.config['LOG_BACKUP_COUNT'])
     #handler.setFormatter(logging.Formatter(app.config['LOG_FORMAT']))
-    #app.logger.addHandler(handler)
+    app.logger.addHandler(handler)
     #context_provider = ContextualFilter()
     #app.logger.addFilter(context_provider)
     return app.logger
