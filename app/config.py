9274b02b6bb1de02900152f3ec9afd7124d43238
Artem Abramov <notdepot@gmail.com>
2022-02-20 18:48:23 +0000

    updated

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config:
     IS_CELERY = sys.argv and sys.argv[0].endswith('celery') and 'worker' in sys.argv
 
     LOG_FILENAME = '/var/log/app/celery.log' if IS_CELERY else '/var/log/app/echidna.log'
-    LOG_FORMAT = '[%(asctime)s] %(method)s [%(url)s] %(levelname)s [%(name)s in %(filename)s, line %(lineno)d: "%(message)s"]'
+    LOG_FORMAT = '[%(asctime)s] %(method)s: [%(url)s] %(levelname)s: [%(name)s in %(filename)s, line %(lineno)d: "%(message)s"]'
     LOG_ROTATE_WHEN = 'H'
     LOG_BACKUP_COUNT = 24
 
